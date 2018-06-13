be17c840b259f3eae3cbec96487ee15b6bbc9340
Alessandro Almeida Filho <alessandro.almeidafilho@gmail.com>
2018-06-13 15:01:04 -0300

    Funcoes JavaScript concluido

---
+++ funcoes/factory1.js
@@ -0,0 +1,10 @@
+// Função Factory é uma função que retorna um objeto, tem como objetivo fabricar uma instância de um objeto
+
+// Factory simples
+
+function criarPessoa(){
+    return{
+        nome: 'Ana',
+        sobrenome: 'Souza'
+    }
+}
