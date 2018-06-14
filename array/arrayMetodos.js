0316b23e462f00e5532028f4c878e1bd8e317ead
Alessandro Almeida Filho <alessandro.almeidafilho@gmail.com>
2018-06-14 16:08:13 -0300

    Adicionando capitulo de arrays

---
+++ array/arrayMetodos.js
@@ -0,0 +1,29 @@
+const pilotos = ['Vettel', 'Alonso', 'Raikkonen', 'Massa']
+pilotos.pop() // remove o último elemento o array no caso "Massa"
+console.log(pilotos)
+
+pilotos.push('Verstappen')
+console.log(pilotos)
+
+pilotos.shift() // remove o primeiro elemento de um array.
+console.log(pilotos)
+
+pilotos.unshift('Hamilton')
+console.log(pilotos)
+
+// splice pode adicionar e remover elementos
+
+//Adicionando
+pilotos.splice(2, 0, 'Bottas', 'Massa')
+console.log(pilotos)
+
+// remover
+pilotos.splice(3, 1)// massa quebrou 
+console.log(pilotos)
+
+const algunsPilotos1 = pilotos.slice(2) // novo array
+console.log(algunsPilotos1)
+
+const algunsPilotos2 = pilotos.slice(1, 4)
+console.log(algunsPilotos2)
+
