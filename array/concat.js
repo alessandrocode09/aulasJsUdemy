3e722a8a674868ba8dce3c7b6193a006e16475d0
Alessandro Almeida Filho <alessandro.almeidafilho@gmail.com>
2018-06-15 12:57:13 -0300

    Adicionando e modificando arquivos capítulo de Arrays

---
+++ array/concat.js
@@ -0,0 +1,6 @@
+const filhas = ['Joana', 'Cibalena']
+const filhos = ['Uoxiton', 'Waldisnei']
+const todos = filhas.concat(filhos, 'Fulano')
+console.log(todos, filhas, filhos)
+
+console.log(['a', 'b'].concat[1,2], [3,4], 5, [[6,7]])
