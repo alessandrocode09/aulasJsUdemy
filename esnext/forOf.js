1a0b05cf69082f473a7bccf558cb063267bc5a10
Alessandro Almeida Filho <alessandro.almeidafilho@gmail.com>
2018-06-18 10:21:19 -0300

    EX aulas Js

---
+++ esnext/forOf.js
@@ -0,0 +1,40 @@
+for (let letra of "Cod3r") {
+    console.log(letra)
+}
+
+const assuntosEcma = ['Map', 'Set', 'Promise'] 
+
+
+for (let i in assuntosEcma) { // percorre os índices
+    console.log(i)
+}
+
+for (let assunto of assuntosEcma){ // percorre os elementos
+    console.log(assunto)
+}
+
+const assuntosMap = new Map ([
+    ['Map', { abordado: true}],
+    ['Set', { abordado: true}],
+    ['Promise', {abordado: false}]
+])
+
+for (let assunto of assuntosMap){
+    console.log(assunto)
+}
+
+for (let chave of assuntosMap.keys()){
+    console.log(chave)
+}
+
+for (let valor of assuntosMap.values()){
+    console.log(valor)
+}
+for (let [ch, vl] of assuntosMap.entries()){ // desestruturando as entradas
+    console.log(ch, vl)
+}
+
+const s = new Set (['a', 'b', 'c'])
+for (let letra of s){
+    console.log(letra)
+}
